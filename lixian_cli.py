e79f9e0b15049177f4fc6ecaa34f57f5a72254b8
Boyu Guo <iambus@gmail.com>
2011-10-06 19:03:01 +0800

    typo fix

--- lixian_cli.py
+++ lixian_cli.py
@@ -267,7 +267,7 @@ def link_equals(x1, x2):
 		x2 = urllib.unquote(x2)
 		if type(x1) == str and type(x2) == unicode:
 			x1 = x1.decode('utf-8')
-		elif type(x1) == unicode and type(x1) == str:
+		elif type(x1) == unicode and type(x2) == str:
 			x2 = x2.decode('utf-8')
 		return x1 == x2
 	else:
