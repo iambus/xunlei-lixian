34bcc0c054dc6a9a806df06190992c51f34b8e4c
Boyu Guo <iambus@gmail.com>
2011-11-29 20:40:07 +0800

    support unicode http url

--- lixian_cli.py
+++ lixian_cli.py
@@ -417,6 +417,18 @@ def download_task(args):
 		else:
 			download_multiple_tasks(client, download, tasks, **download_args)
 
+def normalize_unicode_link(url):
+	def escape_unicode(m):
+		import urllib
+		c = m.group()
+		if ord(c) < 0x80:
+			return c
+		else:
+			return urllib.quote(c.encode('utf-8'))
+	if type(url) == unicode:
+		return re.sub(r'.', escape_unicode, url)
+	return url
+
 def link_equals(x1, x2):
 	if x1.startswith('ed2k://') and x2.startswith('ed2k://'):
 		return lixian_hash_ed2k.parse_ed2k_link(x1) == lixian_hash_ed2k.parse_ed2k_link(x2)
@@ -432,6 +444,9 @@ def link_equals(x1, x2):
 	elif x1.startswith('bt://') and x2.startswith('bt://'):
 		x1 = x1.lower()
 		x2 = x2.lower()
+	elif x1.startswith('http://') and x2.startswith('http://'):
+		x1 = normalize_unicode_link(x1)
+		x2 = normalize_unicode_link(x2)
 	return x1 == x2
 
 def link_in(url, links):
