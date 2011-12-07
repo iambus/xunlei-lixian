4592820157583959924dd8b47438b982b199420f
Boyu Guo <iambus@gmail.com>
2011-12-07 10:23:23 +0800

    call lixian_cli.py as module (not subprocess) from lixian_batch

--- lixian_batch.py
+++ lixian_batch.py
@@ -2,13 +2,13 @@
 
 import sys
 import os.path
-import subprocess
+import lixian_cli
 
 def download_batch(files):
 	for f in map(os.path.abspath, files):
 		print 'Downloading', f, '...'
 		os.chdir(os.path.dirname(f))
-		subprocess.call(['lx', 'download', '--input', f, '--delete', '--continue'])
+		lixian_cli.execute_command(['download', '--input', f, '--delete', '--continue'])
 
 if __name__ == '__main__':
 	download_batch(sys.argv[1:])
