32dc1f6dc05eb08038237355d6e647d821afbf6f
Boyu Guo <iambus@gmail.com>
2011-12-23 18:57:32 +0800

    move command line parser code to lixian_cli_parser

--- lixian_cli.py
+++ lixian_cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from lixian import XunleiClient, encypt_password
+from lixian_cli_parser import parse_command_line
 from lixian_config import *
 import lixian_help
 import lixian_hash
@@ -23,73 +24,6 @@ def to_utf_8(url):
 		return url.decode(default_encoding).encode('utf-8')
 	except:
 		return url
-
-def parse_command_line(args, keys=[], bools=[], alias={}, default={}, help=None):
-	options = {}
-	for k in keys:
-		options[k] = None
-	for k in bools:
-		options[k] = None
-	left = []
-	args = args[:]
-	while args:
-		x = args.pop(0)
-		if x == '--':
-			left.extend(args)
-			break
-		if x.startswith('-'):
-			k = x.lstrip('-')
-			if k in bools:
-				options[k] = True
-			elif k.startswith('no-') and k[3:] in bools:
-				options[k[3:]] = False
-			elif k in keys:
-				options[k] = args.pop(0)
-			elif '=' in k and k[:k.index('=')] in keys:
-				options[k[:k.index('=')]] = k[k.index('=')+1:]
-			elif k in alias:
-				k = alias[k]
-				if k in bools:
-					options[k] = True
-				else:
-					options[k] = args.pop(0)
-			else:
-				if help:
-					print 'Unknown option ' + x
-					print
-					print help
-					exit(1)
-				else:
-					raise RuntimeError('Unknown option '+x)
-		else:
-			left.append(x)
-
-	for k in default:
-		if options[k] is None:
-			options[k] = default[k]
-
-	class Args(object):
-		def __init__(self, args, left):
-			self.__dict__['_args'] = args
-			self.__dict__['_left'] = left
-		def __getattr__(self, k):
-			v = self._args.get(k, None)
-			if v:
-				return v
-			if '_' in k:
-				return self._args.get(k.replace('_', '-'), None)
-		def __setattr__(self, k, v):
-			self._args[k] = v
-		def __getitem__(self, i):
-			if type(i) == int:
-				return self._left[i]
-			else:
-				return self._args[i]
-		def __len__(self):
-			return len(self._left)
-		def __str__(self):
-			return '<Args%s%s>' % (self._args, self._left)
-	return Args(options, left)
 
 def parse_login_command_line(args, keys=[], bools=[], alias={}, default={}, help=None):
 	common_keys = ['username', 'password', 'cookies']
