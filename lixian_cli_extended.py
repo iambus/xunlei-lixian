52cfb4080d9dbb94e3f93b7559ef2ab016bf0500
Boyu Guo <iambus@gmail.com>
2012-09-17 21:21:41 +0800

    check codepage, name.utf-8, and path.utf-8

--- lixian_cli_extended.py
+++ lixian_cli_extended.py
@@ -121,6 +121,23 @@ def extend_links(args):
 
 ##################################################
 
+def b_encoding(b):
+	if 'encoding' in b:
+		return b['encoding']
+	if 'codepage' in b:
+		return 'cp' + str(b['codepage'])
+	return 'utf-8'
+
+def b_name(info, encoding='utf-8'):
+	if 'name.utf-8' in info:
+		return info['name.utf-8'].decode('utf-8')
+	return info['name'].decode(encoding)
+
+def b_path(f, encoding='utf-8'):
+	if 'path.utf-8' in f:
+		return [p.decode('utf-8') for p in f['path.utf-8']]
+	return [p.decode(encoding) for p in f['path']]
+
 @command(usage='list files in local .torrent')
 def list_torrent(args):
 	'''
@@ -131,21 +148,21 @@ def list_torrent(args):
 		with open(p, 'rb') as stream:
 			from lixian_hash_bt import bdecode
 			b = bdecode(stream.read())
-			encoding = b.get('encoding', 'utf-8')
+			encoding = b_encoding(b)
 			info = b['info']
-			print '*', info['name'].decode(encoding).encode(default_encoding)
+			print '*', b_name(info, encoding).encode(default_encoding)
 			if 'files' in info:
 				for f in info['files']:
 					if f['path'][0].startswith('_____padding_file_'):
 						continue
-					path = '/'.join(f['path']).decode(encoding).encode(default_encoding)
+					path = '/'.join(b_path(f, encoding)).encode(default_encoding)
 					if args.size:
 						from lixian_util import format_size
 						print '%s (%s)' % (path, format_size(f['length']))
 					else:
 						print path
 			else:
-				path = info['name'].decode(encoding).encode(default_encoding)
+				path = b_name(info, encoding).encode(default_encoding)
 				if args.size:
 					from lixian_util import format_size
 					print '%s (%s)' % (path, format_size(info['length']))
