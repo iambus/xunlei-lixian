e27c63a97168bfb1432f5e9d498688f74dee42b8
Boyu Guo <iambus@gmail.com>
2013-02-16 23:00:57 +0800

    automatically load lixian_plugins/*.py

--- lixian_plugins/__init__.py
+++ lixian_plugins/__init__.py
@@ -7,5 +7,15 @@ def load_plugins():
 	lixian_query.load_plugin_queries()
 	import lixian_plugins.parsers
 	lixian_plugins.parsers.load_parsers()
+	# load lixian_plugins/*.py
+
+	import os
+	import os.path
+	import re
+	plugin_dir = os.path.dirname(__file__)
+	plugins = os.listdir(plugin_dir)
+	plugins = [re.sub(r'\.py$', '', p) for p in plugins if p.endswith('.py') and not p.startswith('_')]
+	for p in plugins:
+		__import__('lixian_plugins.' + p)
 
 load_plugins()
