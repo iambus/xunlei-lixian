a0afd3b2383e24e8c15e8963b3756964a52b9e23
Boyu Guo <iambus@gmail.com>
2013-02-24 21:07:31 +0800

    fix progress bar (#218)

--- lixian_progress.py
+++ lixian_progress.py
@@ -7,17 +7,18 @@ class SimpleProgressBar:
 	def update(self, percent):
 		self.displayed = True
 		bar_size = 40
-		percent = int(percent*100)
+		percent *= 100.0
 		if percent > 100:
-			percent = 100
-		dots = bar_size * percent / 100
-		plus = percent - dots / bar_size * 100
+			percent = 100.0
+		dots = int(bar_size * percent / 100)
+		plus = percent / 100 * bar_size - dots
 		if plus > 0.8:
 			plus = '='
 		elif plus > 0.4:
-			plu = '>'
+			plus = '-'
 		else:
 			plus = ''
+		percent = int(percent)
 		bar = '=' * dots + plus
 		bar = '{0:>3}%[{1:<40}]'.format(percent, bar)
 		sys.stdout.write('\r'+bar)
