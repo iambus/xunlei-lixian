bf1344795ff688539737628f4db07a9b752c948a
Boyu Guo <iambus@gmail.com>
2013-11-16 14:34:16 +0800

    lx download name:xxx (same as lx download raw:xxx)

--- lixian_plugins/filters/name.py
+++ lixian_plugins/filters/name.py
@@ -1,7 +1,7 @@
 
 from lixian_plugins.api import name_filter
 
-@name_filter(protocol='raw')
+@name_filter(protocol='name')
 def filter_by_raw_text(keyword, name):
 	return keyword.lower() in name.lower()
 
